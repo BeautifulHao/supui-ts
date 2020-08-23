fb0f5be636ce1551bc3f4abcf4ae8cb72785b8c6
BeautifulHao <78714011@qq.com>
2020-08-23 11:30:16 +0800

    storybook

---
+++ src/components/Button/Button.stories.tsx
@@ -0,0 +1,18 @@
+import React from 'react';
+// also exported from '@storybook/react' if you can deal with breaking changes in 6.1
+import { Story, Meta } from '@storybook/react/types-6-0';
+
+import { Button, ButtonProps } from './button';
+
+export default {
+    title: 'Example/Button',
+    component: Button,
+} as Meta;
+
+const Template: Story<ButtonProps> = (args:any) => <Button {...args} />;
+
+export const Primary = Template.bind({});
+Primary.args = {
+    btnType: 'primary',
+    children: 'Button',
+};
